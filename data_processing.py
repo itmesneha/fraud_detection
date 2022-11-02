292cb1bbc5fce78f3dc207bde23d4704c07777c9
itmesneha <reemasneha@gmail.com>
2022-11-02 08:30:53 +0530

    added a new function

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,11 @@
 print('adding code for data processing')
 print('hello new change')
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a + b 
+    return c 
+
+print('added a new function sum  which is not correct')
